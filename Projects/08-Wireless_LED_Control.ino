314f117e06bc1f14a06084ab5661639ce7249f7d
vigneshvaranasi <varanasivsv@gmail.com>
2024-04-30 15:29:38 +0530

    make wifi code short

--- Projects/08-Wireless_LED_Control.ino
+++ Projects/08-Wireless_LED_Control.ino
@@ -3,44 +3,29 @@
 const char *ssid = "VVSV";         // Your Wi-Fi Name
 const char *password = "varanasi"; // Wi-Fi Password
 int LED = 2;                       // led connected to GPIO2 (D4)
-WiFiServer server(80);             // Fixed typo in WiFiServer declaration
+WiFiServer server(80);           
 
 void setup()
 {
-    Serial.begin(115200); // Default Baudrate
+    Serial.begin(115200); 
     pinMode(LED, OUTPUT);
     digitalWrite(LED, LOW);
-    Serial.print("Connecting to the Network");
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.print(".");
     }
-    Serial.println("WiFi connected");
+    Serial.println("\nWiFi connected");
     server.begin(); // Starts the Server
-    Serial.println("Server started");
-    Serial.print("IP Address of network:"); // will IP address on Serial Monitor
+    Serial.print("IP Address of network:"); // will print IP address on Serial Monitor
     Serial.println(WiFi.localIP());
-    Serial.print("Copy and paste the following URL: http://"); // Will print IP address in URL format
-    Serial.print(WiFi.localIP());
-    Serial.println("/");
 }
 
 void loop()
 {
     WiFiClient client = server.available();
-    if (!client)
-    {
-        return;
-    }
-    Serial.println("Waiting for new client");
-    while (!client.available())
-    {
-        delay(1);
-    }
     String request = client.readStringUntil('\r');
-    Serial.println(request);
     client.flush();
     int value = LOW;
     if (request.indexOf("/LED=ON") != -1)
@@ -55,25 +40,17 @@ void loop()
     }
 
     // HTML Page Code
-    client.println("HTTP/1.1 200 OK");         // Fixed spacing issue and added correct HTTP response header
-    client.println("Content-Type: text/html"); // Fixed Content-Type header
-    client.println();
     client.println("<!DOCTYPE HTML>");
     client.println("<html>");
     client.print("CONTROL LED:");
+
     if (value == HIGH)
-    {
         client.print("ON");
-    }
     else
-    {
         client.print("OFF");
-    }
-    client.println("<br><br>");
+
+    client.println("<br>");
     client.println("<a href=\"/LED=ON\"><button>ON</button></a>");
-    client.println("<a href=\"/LED=OFF\"><button>OFF</button></a><br/>"); // Fixed typo in anchor tag
+    client.println("<a href=\"/LED=OFF\"><button>OFF</button></a>");
     client.println("</html>");
-    delay(1);
-    Serial.println("Client disconnected"); // Fixed typo in Serial.println
-    Serial.println("");
 }
