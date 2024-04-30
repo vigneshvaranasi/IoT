2d1692cc968c11733a03ea49a5caf8908f705971
vigneshvaranasi <varanasivsv@gmail.com>
2024-04-30 15:54:06 +0530

    Refactor WiFi code and update comments in 10-2-MIT_Home_Automation.ino

--- Projects/10-2-MIT_Home_Automation.ino
+++ Projects/10-2-MIT_Home_Automation.ino
@@ -1,37 +1,28 @@
 #include <ThingSpeak.h>
-
-#include <ThingSpeak.h>               // add librery
 #include <ESP8266WiFi.h>
 
-WiFiClient  client;
-unsigned long counterChannelNumber = 2487036;                // Channel ID
-const char * myCounterReadAPIKey = "UEW8WV4NCGK3XIBK";      // Read API Key
-const int FieldNumber1 = 1;                                 // The field you wish to read
-const int FieldNumber2 = 2;                                 // The field you wish to read
+WiFiClient client;
+unsigned long counterChannelNumber = 2487036;         // Channel ID
+const char *myCounterReadAPIKey = "UEW8WV4NCGK3XIBK"; // Read API Key
+const int FieldNumber1 = 1;                           // The field you wish to read
+const int FieldNumber2 = 2;                           // The field you wish to read
 
 void setup()
 {
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT); // D7
   Serial.begin(115200);
-  Serial.println();
-
-  WiFi.begin("iPhone", "vigneshvaranasi");                 // write wifi name & password           
-
-  Serial.print("Connecting");
+  WiFi.begin("VVSV", "varanasi"); // write wifi name & password
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("\nWIFI Connected");
   ThingSpeak.begin(client);
 }
 
-void loop() 
+void loop()
 {
- int A = ThingSpeak.readLongField(counterChannelNumber, FieldNumber1, myCounterReadAPIKey);
- Serial.println(A);
- digitalWrite(13,A);
+  int A = ThingSpeak.readLongField(counterChannelNumber, FieldNumber1, myCounterReadAPIKey);
+  digitalWrite(13, A);
 }
