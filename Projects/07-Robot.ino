1ab902cdc8f7e37cc744f643ea233ef759612a9d
vigneshvaranasi <varanasivsv@gmail.com>
2024-04-30 20:48:20 +0530

    Refactor motor control code for better readability and maintainability

--- Projects/07-Robot.ino
+++ Projects/07-Robot.ino
@@ -1,60 +1,33 @@
-int trigPin = 9;    // trig pin of HC-SR04
-int echoPin = 8;    // Echo pin of HC-SR04
-int revleft = 10;   // REVerse motion of Left motor
-int fwdleft = 11;   // ForWarD motion of Left motor
-int revright = 12;  // REVerse motion of Right motor
-int fwdright = 13;  // ForWarD motion of Right motor
-
-long duration, distance;
+#include <AFMotor.h>
+const int motorSpeed = 200;
+AF_DCMotor motorG(1); 
+AF_DCMotor motorM(2);
 
 void setup() {
-  Serial.begin(9600);
-  
-  pinMode(revleft, OUTPUT);
-  pinMode(fwdleft, OUTPUT);
-  pinMode(revright, OUTPUT);
-  pinMode(fwdright, OUTPUT);
-  
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  motorG.setSpeed(motorSpeed);
+  motorM.setSpeed(motorSpeed);
+  motorG.run(RELEASE);
+  motorM.run(RELEASE);
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration / 58.2;
-  
-  if (distance > 19) {
-    digitalWrite(fwdright, HIGH);
-    digitalWrite(revright, LOW);
-    digitalWrite(fwdleft, HIGH);
-    digitalWrite(revleft, LOW);
-  } else if (distance < 18) {
-    digitalWrite(fwdright, LOW);
-    digitalWrite(revright, LOW);
-    digitalWrite(fwdleft, LOW);
-    digitalWrite(revleft, LOW);
-    delay(500);
-    
-    digitalWrite(fwdright, LOW);
-    digitalWrite(revright, HIGH);
-    digitalWrite(fwdleft, LOW);
-    digitalWrite(revleft, HIGH);
-    delay(500);
-    
-    digitalWrite(fwdright, LOW);
-    digitalWrite(revright, LOW);
-    digitalWrite(fwdleft, LOW);
-    digitalWrite(revleft, LOW);
-    delay(100);
-    
-    digitalWrite(fwdright, HIGH);
-    digitalWrite(revright, LOW);
-    digitalWrite(revleft, LOW);
-    digitalWrite(fwdleft, LOW);
-    delay(500);
-  }
+  motorG.run(FORWARD);
+  motorM.run(FORWARD);
+  delay(5000);
+
+  motorG.run(BACKWARD);
+  motorM.run(BACKWARD);
+  delay(5000);
+
+  motorG.run(FORWARD);
+  motorM.run(RELEASE);
+  delay(500);
+
+  motorG.run(RELEASE);
+  motorM.run(FORWARD);
+  delay(500);
+
+  motorG.run(RELEASE);
+  motorM.run(RELEASE);
+  delay(500);
 }
